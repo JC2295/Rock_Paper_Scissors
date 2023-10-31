79200e095c1046f9e4c032779d8e72c1c8827579
JC2295 <64856330+JC2295@users.noreply.github.com>
2023-10-31 11:16:22 +0000

    Completed program.

--- index.js
+++ index.js
@@ -1,25 +1,84 @@
 function getComputerChoice() {
 
-  let options = ["Rock", "Paper", "Scissors"];
+  let options = ["rock", "paper", "scissors"];
 
-  let selection = Math.floor(Math.random() * max);
+  let selection = Math.floor(Math.random() * 3);
 
   return options[selection];
 }
 
 function playRound(playerSelection, computerSelection) {
   // your code here!
-  if (playerSelection == "Rock" && computerSelection == "Scissors") {
-    player
+  let playerSelectionLower = playerSelection.toLowerCase();
+
+  if (playerSelectionLower == "rock" && computerSelection == "scissors") {
+    console.log("You win, rock beats scissors");
+    return "won";
+  }
+  else if ((playerSelectionLower == "rock" && computerSelection == "paper")) {
+    console.log("You lose, paper beats rock");
+    return "lost";
+  }
+  else if ((playerSelectionLower == "rock" && computerSelection == "rock")) {
+    console.log("It's a draw");
+    return "drew";
+  }
+  else if ((playerSelectionLower == "paper" && computerSelection == "rock")) {
+    console.log("You win, paper beats rock");
+    return "won";
+  }
+  else if ((playerSelectionLower == "paper" && computerSelection == "paper")) {
+    console.log("It's a draw");
+    return "drew";
+  }
+  else if ((playerSelectionLower == "paper" && computerSelection == "scissors")) {
+    console.log("You lose, scissors beats paper");
+    return "lost";
+  }
+  else if ((playerSelectionLower == "scissors" && computerSelection == "rock")) {
+    console.log("You lose, rock beats scissors");
+    return "lost";
+  }
+  else if ((playerSelectionLower == "scissors" && computerSelection == "paper")) {
+    console.log("You win, scissors beats paper");
+    return "won";
+  }
+  else if ((playerSelectionLower == "scissors" && computerSelection == "scissors")) {
+    console.log("It's a draw");
+    return "drew";
   }
 }
-
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
 
 function game() {
+
+  let playerScore = 0;
+  let computerScore = 0;
+
   for (let i = 0; i < 5; i++) {
-    playRound(playerSelection, computerSelection)
+
+    playerSelection = prompt("Enter Option: ");
+
+    computerSelection = getComputerChoice();
+
+    let result = playRound(playerSelection, computerSelection);
+
+    if (result == "won") {
+      playerScore++;
+    }
+    else if (result == "lost") {
+      computerScore++;
+    }
   }
+
+  if (playerScore > computerScore) {
+    return "Player is the overall winner";
+  }
+  else if (computerScore > playerScore) {
+    return "Computer is the overall winner"
+  }
+  else if (computerScore == playerScore)
+    return "It's a draw";
 }
+
+console.log(game());
+
